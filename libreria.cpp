55c068aa01df5a72a2b1f7529460799ee214a874
simogiliberto <100995782+simogiliberto@users.noreply.github.com>
2022-05-04 17:11:13 +0200

    Add files via upload

---
+++ libreria.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Anag
+{
+  
+  string titolo, autore,editore,anno_pubb,codice,genere;
+  float prezzo;
+ 
+};
+
+int inserimento(Anag libro[])
+{
+  int n;
+  cout<<"inserisci nuovo libro"<<endl;
+  cin>>n;
+
+  for(int i = 0; i < n; i++)
+  {
+    cout<<"inserisci il codice ISBN"<<endl;
+    cin>> libro[i].codice;
+    cout<<"TITOLO del libro"<<endl;
+    cin>> libro[i].titolo;
+    cout<<"Autore"<<endl;
+    cin>>libro[i].autore;
+    cout<<"Editore"<<endl;
+    cin>>libro[i].editore;
+    cout<<"Anno di Pubblicazione"<<endl;
+    cin>>libro[i].anno_pubb;
+    cout<<" Tag del Genere"<<endl;
+    cin>>libro[i].genere;
+    cout<<"Prezzo"<<endl;
+    cin>>libro[i].prezzo;
+    cout<<endl;
+  }
+  return n;
+}
+
